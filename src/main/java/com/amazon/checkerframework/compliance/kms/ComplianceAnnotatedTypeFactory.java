f74693973ad6a9ac0c0f52c553daaab0b68e6033
Martin Kellogg <kelloggm@cs.washington.edu>
2020-02-27 11:32:37 -0800

    update to latest versions of gradle, CF. Make this build w/o deployment keys. Remove some code that worked around a CF bug that has been fixed

--- src/main/java/com/amazon/checkerframework/compliance/kms/ComplianceAnnotatedTypeFactory.java
+++ src/main/java/com/amazon/checkerframework/compliance/kms/ComplianceAnnotatedTypeFactory.java
@@ -1,54 +1,18 @@
 package com.amazon.checkerframework.compliance.kms;
 
-import com.sun.source.tree.MemberSelectTree;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.common.value.ValueAnnotatedTypeFactory;
-import org.checkerframework.framework.type.AnnotatedTypeMirror;
-import org.checkerframework.framework.type.treeannotator.ListTreeAnnotator;
-import org.checkerframework.framework.type.treeannotator.TreeAnnotator;
-
-import java.util.Collections;
 
 /**
- * Adds new type introduction rules to give specific type annotations to DataKeySpec
- * enums.
+ * Empty annotated type factory, in case we ever need one.
+ *
+ * Used to handle defaulting for enums until https://github.com/typetools/checker-framework/issues/2147
+ * was fixed.
  */
 public class ComplianceAnnotatedTypeFactory extends ValueAnnotatedTypeFactory {
 
     public ComplianceAnnotatedTypeFactory(BaseTypeChecker checker) {
         super(checker);
         super.postInit();
-    }
-
-    @Override
-    public TreeAnnotator createTreeAnnotator() {
-        return new ListTreeAnnotator(
-                new ComplianceTreeAnnotator(this), super.createTreeAnnotator());
-    }
-
-    /**
-     * The tree annotator is responsible for placing default annotations on trees.
-     */
-    private class ComplianceTreeAnnotator extends TreeAnnotator {
-
-        private static final String DATA_KEY_SPEC = "com.amazonaws.services.kms.model.DataKeySpec";
-
-        ComplianceTreeAnnotator(ComplianceAnnotatedTypeFactory typeFactory) {
-            super(typeFactory);
-        }
-
-        /**
-         * The type of a value of the DataKeySpec enum is a StringVal with a
-         * value equal to the name of the member. So, for example, DataKeySpec.AES_256's
-         * type is @StringVal("AES_256").
-         */
-        @Override
-        public Void visitMemberSelect(MemberSelectTree tree, AnnotatedTypeMirror type) {
-            if (DATA_KEY_SPEC.equals(getAnnotatedType(tree.getExpression()).getUnderlyingType().toString())) {
-                String identifier = tree.getIdentifier().toString();
-                type.replaceAnnotation(createStringAnnotation(Collections.singletonList(identifier)));
-            }
-            return super.visitMemberSelect(tree, type);
-        }
     }
 }
